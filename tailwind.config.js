d76474c29dc2bdb79537a88f50a2d1a69dcd1a9a
yahoogm <yaho.garfield@gmail.com>
2023-04-20 23:48:43 +0700

    config: setting daisyui theme to light

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,7 @@ module.exports = {
   theme: {},
 
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: '#6750A4',
-          secondary: '#625B71',
-          accent: '#7E5260',
-          neutral: '#191D24',
-          'base-100': '#2A303C',
-          info: '#3ABFF8',
-          success: '#36D399',
-          warning: '#FBBD23',
-          error: '#BA1A1A',
-        },
-      },
-    ],
+    themes: ['light'],
   },
   plugins: [require('daisyui')],
 };
