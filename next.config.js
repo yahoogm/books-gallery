9ee1d5329ca085db27516da66f5d5571f01a7ab7
yahoogm <yaho.garfield@gmail.com>
2023-04-23 01:16:54 +0700

    config: add env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    API_URL: process.env.NEXT_PUBLIC_API_URL,
+    API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
