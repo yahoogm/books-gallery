2b7ea61a11cc9983262a6b48aaf01996e5361d0b
yahoogm <yaho.garfield@gmail.com>
2023-04-24 23:45:59 +0700

    next config: add images domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['books.google.com'],
+  },
   env: {
     API_URL: process.env.NEXT_PUBLIC_API_URL,
     API_KEY: process.env.NEXT_PUBLIC_API_KEY,
