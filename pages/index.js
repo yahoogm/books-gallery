543d9975430d322caec8e314ec79d464335fcb05
yahoogm <yaho.garfield@gmail.com>
2023-04-24 23:48:27 +0700

    pages: add next head

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import CardContent from '@/components/organisme/CardContent/CardContent.organism';
 import HeroContent from '@/components/organisme/HeroContent/HeroContent.organism';
 import HeaderAndFooter from '@/components/templates/HeaderAndFooter/HeaderAndFooter.template';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <HeaderAndFooter>
+      <Head>
+        <title>Books Gallery</title>
+      </Head>
       <HeroContent />
       <CardContent />
     </HeaderAndFooter>
