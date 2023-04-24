8f133b925460ebaec44fdd599de635451a143d26
yahoogm <yaho.garfield@gmail.com>
2023-04-24 23:48:07 +0700

    components: refactor code

--- components/molecules/Card/Card.molecule.jsx
+++ components/molecules/Card/Card.molecule.jsx
@@ -1,27 +1,38 @@
 import Button from '@/components/atoms/Button/Button.atom';
+import Image from 'next/image';
+import { HeroImage } from '@/public/assets';
 
-const Card = ({ title = 'null', author = 'null', totalEdition = 'null', firstPublish = 'null' }) => {
+const Card = ({ title = 'Not found', authors = ['Not found'], publisher = 'Not found', publishedDate = 'Not found', cover = HeroImage }) => {
+  // handle nama author jika lebih dari 1
+  const modifiedName = authors?.map((author, idx) => {
+    if (idx !== authors.length - 1) {
+      return author + ',' + '';
+    }
+    return author;
+  });
+
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
-        <img src="./assets/hero/books.jpg" alt="Shoes" />
+        <Image src={cover} alt="Shoes" width={500} height={300} priority={true} className="object-fill w-full h-48" onError={HeroImage} />
       </figure>
       <div className="card-body">
         <h2 className="card-title line-clamp-1">{title}</h2>
 
         <div className="grid grid-cols-5 space-x-2">
           <span className="col-span-1">Author :</span>
-          <span className="truncate col-span-4">{author}</span>
+
+          <span className="truncate col-span-4 space-x-2">{authors.length != null || undefined ? modifiedName : 'null'}</span>
         </div>
 
         <div className="grid grid-cols-3 space-x-2">
           <span className="col-span-1">Total Edition :</span>
-          <span className="truncate col-span-2">{totalEdition}</span>
+          <span className="truncate col-span-2">{publisher}</span>
         </div>
 
-        <div className="grid grid-cols-3 space-x-2">
-          <span className="col-span-1">First Publish :</span>
-          <span className="truncate col-span-2">{firstPublish}</span>
+        <div className="grid grid-cols-5 space-x-2">
+          <span className="col-span-2">Published Date :</span>
+          <span className="truncate col-span-3">{publishedDate}</span>
         </div>
 
         <div className="card-actions justify-end">
