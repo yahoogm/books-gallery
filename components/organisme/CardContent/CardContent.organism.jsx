bdb626b487d63056ec3a07fe347e0247520d1cfb
yahoogm <yaho.garfield@gmail.com>
2023-04-24 23:47:47 +0700

    components: refactor code

--- components/organisme/CardContent/CardContent.organism.jsx
+++ components/organisme/CardContent/CardContent.organism.jsx
@@ -1,24 +1,31 @@
 import Card from '@/components/molecules/Card/Card.molecule';
-import { useSearchSelector, useSearchTypeSelector } from '@/config/redux/search/searchSelector.reducer';
-import { retrieveSearch } from '@/config/redux/search/searchThunk.reducer';
 import CardLoadingAnimation from '@/components/organisme/CardLoadingAnimation/CardLoadingAnimation.organism';
+import useCardContentOrganismModel from './CardContent.organismModel';
 
 const CardContent = () => {
-  const search = useSearchSelector();
-  const type = useSearchTypeSelector();
-
-  if (type === retrieveSearch.pending.type) {
-    return <CardLoadingAnimation />;
-  }
+  const cardModel = useCardContentOrganismModel();
 
   return (
     <div>
       <h2 className="text-center mb-10 text-4xl font-semibold">Your Search Books</h2>
-      <div className="grid grid-cols-3 gap-14">
-        {search.items?.map((search, idx) => {
-          return <Card key={idx.toString()} title={search.volumeInfo.title} />;
-        })}
-      </div>
+      {cardModel.loading ? (
+        <CardLoadingAnimation />
+      ) : (
+        <div className="grid grid-cols-3 gap-14">
+          {cardModel.search?.items?.map((search, idx) => {
+            return (
+              <Card
+                key={idx.toString()}
+                title={search.volumeInfo.title}
+                authors={search.volumeInfo?.authors}
+                cover={search.volumeInfo.imageLinks?.thumbnail}
+                publishedDate={search.volumeInfo?.publishedDate}
+                publisher={search.volumeInfo?.publisher}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
