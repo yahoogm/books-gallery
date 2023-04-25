a1b59db6096ec0623a1cb638a3529640aed095f3
yahoogm <yaho.garfield@gmail.com>
2023-04-25 17:43:37 +0700

    Renaming files

--- config/redux/search/searchSelector.reducer.jsx
+++
@@ -1,5 +0,0 @@
-import { useSelector } from 'react-redux';
-
-export const useSearchSelector = () => useSelector((state) => state.search.search);
-
-export const useSearchTypeSelector = () => useSelector((state) => state.search.type);
--- config/redux/search/searchThunk.reducer.jsx
+++
@@ -1,7 +0,0 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-
-export const retrieveSearch = createAsyncThunk('product/retrieveSearch', async (param) => {
-  const res = await axios.get(`${process.env.API_URL}/volumes?q=intitle:${param.search}&key=${process.env.API_KEY}`);
-  return res.data;
-});
