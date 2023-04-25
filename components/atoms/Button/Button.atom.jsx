c90f0f2e816775c1b8eaaae7d7956d9029b5ec75
yahoogm <yaho.garfield@gmail.com>
2023-04-25 17:38:23 +0700

    component: add prop onclick

--- components/atoms/Button/Button.atom.jsx
+++ components/atoms/Button/Button.atom.jsx
@@ -1,6 +1,6 @@
-const Button = ({ text, variant, type }) => {
+const Button = ({ text, variant, type, onClick }) => {
   return (
-    <button className={`btn ${variant}`} type={type}>
+    <button className={`btn ${variant}`} type={type} onClick={onClick}>
       {text}
     </button>
   );
