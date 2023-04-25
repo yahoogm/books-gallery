cd18dc0ef8b65bf433b24467510f5a961a820c4c
yahoogm <yaho.garfield@gmail.com>
2023-04-25 23:51:47 +0700

    Add selector detail book

--- config/redux/books/bookSelector.reducer.jsx
+++ config/redux/books/bookSelector.reducer.jsx
@@ -3,3 +3,5 @@ import { useSelector } from 'react-redux';
 export const useSearchBooksSelector = () => useSelector((state) => state.book.books);
 
 export const useSearchBooksTypeSelector = () => useSelector((state) => state.book.type);
+
+export const useDetailBook = () => useSelector((state) => state.book.detailBook);
