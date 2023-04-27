cd076a51186a9424300889ca6d31fe3324ab6991
yahoogm <yaho.garfield@gmail.com>
2023-04-27 22:10:13 +0700

    Add component ToastContainer

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import { Store } from '@/config/redux/store/store';
 import { Provider } from 'react-redux';
 import '@/styles/globals.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={Store}>
+      <ToastContainer position="top-right" autoClose={3000} pauseOnHover={false} theme="colored" pauseOnFocusLoss={false} />
       <Component {...pageProps} />
     </Provider>
   );
