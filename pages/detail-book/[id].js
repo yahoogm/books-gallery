ce2356f630fab818ae5114a7f8f98637271a230c
yahoogm <yaho.garfield@gmail.com>
2023-04-30 16:06:27 +0700

    Add import useDetailBookModel to use model for logic, add useEffect to handle the read book, add tag script to download function from google books api for embed the book, add div to handle the view container

--- pages/detail-book/[id].js
+++ pages/detail-book/[id].js
@@ -1,20 +1,45 @@
 import HeaderAndFooter from '@/components/templates/HeaderAndFooter/HeaderAndFooter.template';
 import Head from 'next/head';
 import axios from 'axios';
-import { addDetailBook } from '@/config/redux/books/bookSlice.reducer';
+import { addReadBook } from '@/config/redux/books/bookSlice.reducer';
 import { useDispatch } from 'react-redux';
+import Script from 'next/script';
 import BookDetailContent from '@/components/organisme/BookDetailContent/BookDetailContent.organism';
+import useDetailBookModel from './DetailBook.pageModel';
+import { useEffect } from 'react';
 
 const DetailBook = ({ book }) => {
+  const model = useDetailBookModel();
   const dispatch = useDispatch();
-  dispatch(addDetailBook(book));
+
+  useEffect(() => {
+    const bookId = book.id;
+    const industryIdentifiers = book.volumeInfo.industryIdentifiers;
+
+    if (bookId && industryIdentifiers) {
+      dispatch(addReadBook([bookId, 'ISBN:' + industryIdentifiers[0].identifier]));
+    } else {
+      dispatch(addReadBook([bookId]));
+    }
+  }, [book.id, book.volumeInfo.industryIdentifiers, dispatch]);
 
   return (
     <HeaderAndFooter>
       <Head>
         <title>Detail Book</title>
       </Head>
-      <BookDetailContent />
+      <BookDetailContent book={book} showBook={model.initialize} />
+      <Script
+        id="google-books-script"
+        src="https://www.google.com/books/jsapi.js"
+        strategy="afterInteractive"
+        onLoad={() => {
+          google.books.load(); // eslint-disable-line no-undef
+          google.books.setOnLoadCallback(() => model.initialize); // eslint-disable-line no-undef
+        }}
+      />
+
+      <div ref={model.canvasRef} id="viewerCanvas" className="w-full h-[700px] m-auto"></div>
     </HeaderAndFooter>
   );
 };
