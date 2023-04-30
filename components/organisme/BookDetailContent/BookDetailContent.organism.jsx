5c623885af9f20070a31863676d49a28c48057c4
yahoogm <yaho.garfield@gmail.com>
2023-04-30 16:00:16 +0700

    Refactoring code

--- components/organisme/BookDetailContent/BookDetailContent.organism.jsx
+++ components/organisme/BookDetailContent/BookDetailContent.organism.jsx
@@ -1,14 +1,16 @@
 import CardImage from '@/components/molecules/CardImage/CardImage.molecule';
 import CardInformation from '@/components/molecules/CardInformation/CardInformation.molecule';
-import useBookDetailContentOrganismModel from './BookDetailContent.organismModel';
+import { modifiedName } from '@/config/functions/Functions';
 
-const BookDetailContent = () => {
-  const model = useBookDetailContentOrganismModel();
+const BookDetailContent = ({ book, showBook }) => {
+  const authorsName = modifiedName(book.volumeInfo.authors);
+  const categoriesName = modifiedName(book.volumeInfo.categories);
+
   return (
     <div className="flex w-full space-x-5">
-      <CardImage volume={model.bookDetail.volumeInfo} />
+      <CardImage volume={book.volumeInfo} />
       <div className="divider divider-horizontal"></div>
-      <CardInformation volume={model.bookDetail.volumeInfo} authorsName={model.authorsName} categoriesName={model.categoriesName} id={model.bookDetail.id} />
+      <CardInformation volume={book.volumeInfo} authorsName={authorsName} categoriesName={categoriesName} showBook={showBook} />
     </div>
   );
 };
