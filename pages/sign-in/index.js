3ca80987bf8ddd0dee4721b1761299013785d766
yahoogm <yaho.garfield@gmail.com>
2023-05-03 00:58:15 +0700

    Add page sign in

---
+++ pages/sign-in/index.js
@@ -0,0 +1,12 @@
+import CardSignIn from '@/components/organisme/CardSignIn/CardSignIn.organism';
+import HeaderAndFooter from '@/components/templates/HeaderAndFooter/HeaderAndFooter.template';
+
+const SignIn = () => {
+  return (
+    <HeaderAndFooter>
+      <CardSignIn />
+    </HeaderAndFooter>
+  );
+};
+
+export default SignIn;
