3f81f37e77588d38f5ae1e09a6e3ae6c84eb513d
yahoogm <yaho.garfield@gmail.com>
2023-05-03 23:04:24 +0700

    Add selector to check if user login or not

--- config/redux/user/userSelector.reducer.jsx
+++ config/redux/user/userSelector.reducer.jsx
@@ -1,3 +1,6 @@
 import { useSelector } from 'react-redux';
 
 export const useUserSelector = () => useSelector((state) => state.user.user);
+
+export const useIsLoginSelector = () =>
+  useSelector((state) => state.user.isLogin);
