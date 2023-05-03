835e50e2b84fd35df8d81aa8d3c986cc9b3345a9
yahoogm <yaho.garfield@gmail.com>
2023-05-03 23:01:11 +0700

    Add validation to show the UI

--- components/molecules/Header/Header.molecule.jsx
+++ components/molecules/Header/Header.molecule.jsx
@@ -1,7 +1,7 @@
 import Title from '@/components/atoms/Title/Title.atom';
 import Link from 'next/link';
 
-const Header = () => {
+const Header = ({ isLogin, user, logout, dispatch }) => {
   return (
     <div className="navbar px-14 py-6">
       <div className="flex-1 space-x-20">
@@ -13,13 +13,38 @@ const Header = () => {
           About
         </a>
       </div>
-      <div className="flex-none">
-        <div className="dropdown dropdown-end">
-          <Link href="/sign-in" className="btn btn-primary">
-            Sign In
-          </Link>
+
+      {isLogin !== true ? (
+        <div className="flex-none">
+          <div className="dropdown dropdown-end">
+            <Link href="/sign-in" className="btn btn-primary">
+              Sign In
+            </Link>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="space-x-4">
+          <div>
+            <span className="font-semibold">{user.name}</span>
+          </div>
+
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src={user.profilePic} />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li onClick={() => dispatch(logout({}))}>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
