ca8f74225274fa4a80d21bf0fd95e986a51eaeb0
yahoogm <yaho.garfield@gmail.com>
2023-05-03 21:20:15 +0700

    Add user reducer

--- config/redux/reducers/reducers.js
+++ config/redux/reducers/reducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import searchReducer from '../books/bookSlice.reducer';
+import userReducer from '../user/userSlice.reducer';
 
 export const Reducers = combineReducers({
   book: searchReducer,
+  user: userReducer,
 });
