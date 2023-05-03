bd3eecd93b011522421956017be6142189c5fb0a
yahoogm <yaho.garfield@gmail.com>
2023-05-03 21:19:36 +0700

    Refactor tag

--- components/organisme/CardSignIn/CardSignIn.organism.jsx
+++ components/organisme/CardSignIn/CardSignIn.organism.jsx
@@ -1,13 +1,8 @@
 import FormSignIn from '@/components/molecules/FormSignIn/FormSignIn.molecule';
-import Image from 'next/image';
-import { HeroImage } from '@/public/assets';
 
 const CardSignIn = () => {
   return (
-    <div className="card lg:card-side bg-base-100 shadow-xl">
-      <div className="w-[600px] h-[600px] relative">
-        <Image src={HeroImage} priority={true} fill sizes={'100vh'} alt={'Detail Book'} className="rounded-l-lg  shadow-md object-fill" onError={() => HeroImage} />
-      </div>
+    <div className="card w-96 h-96 bg-secondary shadow-xl m-auto">
       <div className="m-auto">
         <FormSignIn />
       </div>
