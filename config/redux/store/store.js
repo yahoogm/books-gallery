eb3e7d29c7f8373375bfe797e547387b8b05753d
yahoogm <yaho.garfield@gmail.com>
2023-05-03 21:21:07 +0700

    Add thunk middleware and redux persist, and make slice user to local storage

--- config/redux/store/store.js
+++ config/redux/store/store.js
@@ -1,6 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { Reducers } from '@/config/redux/reducers/reducers';
+import thunkMiddleware from 'redux-thunk';
 
-export const Store = configureStore({
-  reducer: Reducers,
+const persistConfig = { key: 'root', storage, whitelist: ['user'] };
+
+const persistatedReducers = persistReducer(persistConfig, Reducers);
+
+const Store = configureStore({
+  reducer: persistatedReducers,
+  middleware: [thunkMiddleware],
 });
+
+export const persiststore = persistStore(Store);
+
+export default Store;
