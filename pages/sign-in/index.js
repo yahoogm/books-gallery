1a55b6f61798fc197f6f2cb9c176bb8d46915d6c
yahoogm <yaho.garfield@gmail.com>
2023-05-03 21:22:13 +0700

    Add tag head for title in web

--- pages/sign-in/index.js
+++ pages/sign-in/index.js
@@ -1,9 +1,13 @@
 import CardSignIn from '@/components/organisme/CardSignIn/CardSignIn.organism';
 import HeaderAndFooter from '@/components/templates/HeaderAndFooter/HeaderAndFooter.template';
+import Head from 'next/head';
 
 const SignIn = () => {
   return (
     <HeaderAndFooter>
+      <Head>
+        <title>Sign In</title>
+      </Head>
       <CardSignIn />
     </HeaderAndFooter>
   );
