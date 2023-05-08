6ffc0b526af946abe9ee435f26cc694ee047ce44
yahoogm <yaho.garfield@gmail.com>
2023-05-09 00:08:23 +0700

    Delete button

--- components/molecules/CardInformation/CardInformation.molecule.jsx
+++ components/molecules/CardInformation/CardInformation.molecule.jsx
@@ -1,20 +1,56 @@
 import Button from '@/components/atoms/Button/Button.atom';
 import Information from '@/components/atoms/Information/Information.atom';
+import Modal from '@/components/molecules/Modal/Modal.atom';
 
 const CardInformation = ({ volume, authorsName, categoriesName, showBook }) => {
   return (
     <div className="grid flex-grow rounded-box shadow-md items-center p-10">
       <div className="space-y-6">
-        <h2 className="font-semibold text-2xl">{volume?.title ? volume.title : 'Not found'}</h2>
+        <h2 className="font-semibold text-2xl">
+          {volume?.title ? volume.title : 'Not found'}
+        </h2>
         <div className="space-y-2">
-          <Information title={'Author'} information={volume?.authors?.length != null || undefined ? authorsName : 'Not found'} />
-          <Information title={'Publisher'} information={volume?.publisher ? volume.publisher : 'Not Found'} />
-          <Information title={'Published Date'} information={volume?.publishedDate ? volume.publishedDate : 'Not Found'} />
-          <Information title={'Page Count'} information={volume?.pageCount ? volume.pageCount : 'Not Found'} />
-          <Information title={'Category'} information={categoriesName ? categoriesName : 'Not found'} />
-          <Information title={'Description'} information={volume.description ? volume.description : 'Not Found'} />
+          <Information
+            title={'Author'}
+            information={
+              volume?.authors?.length != null || undefined
+                ? authorsName
+                : 'Not found'
+            }
+          />
+          <Information
+            title={'Publisher'}
+            information={volume?.publisher ? volume.publisher : 'Not Found'}
+          />
+          <Information
+            title={'Published Date'}
+            information={
+              volume?.publishedDate ? volume.publishedDate : 'Not Found'
+            }
+          />
+          <Information
+            title={'Page Count'}
+            information={volume?.pageCount ? volume.pageCount : 'Not Found'}
+          />
+          <Information
+            title={'Category'}
+            information={categoriesName ? categoriesName : 'Not found'}
+          />
+          <Information
+            title={'Description'}
+            information={volume.description ? volume.description : 'Not Found'}
+          />
         </div>
-        <Button text={'read book'} type={'button'} variant={'btn-primary'} onClick={() => showBook()} />
+        <div className="space-x-4 flex">
+          <Button
+            text={'read book'}
+            type={'button'}
+            variant={'btn-primary'}
+            onClick={() => showBook()}
+          />
+
+          <Modal btnTitle={'add review'} modalTitle={'make review'} />
+        </div>
       </div>
     </div>
   );
