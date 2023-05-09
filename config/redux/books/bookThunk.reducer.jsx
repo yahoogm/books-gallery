feab70d071d24615f8404b97a452bcce5367edb9
yahoogm <yaho.garfield@gmail.com>
2023-05-10 00:00:19 +0700

    add variable handle async function retrieveReviewBook

--- config/redux/books/bookThunk.reducer.jsx
+++ config/redux/books/bookThunk.reducer.jsx
@@ -1,7 +1,27 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { db } from '@/config/firebase/sdk/sdk';
+import { collection, getDocs } from 'firebase/firestore';
 
-export const retrieveSearchBooks = createAsyncThunk('product/retrieveSearch', async (param) => {
-  const res = await axios.get(`${process.env.API_URL}/volumes?q=intitle:${param.search}&key=${process.env.API_KEY}`);
-  return res.data;
-});
+export const retrieveSearchBooks = createAsyncThunk(
+  'product/retrieveSearch',
+  async (param) => {
+    const res = await axios.get(
+      `${process.env.API_URL}/volumes?q=intitle:${param.search}&key=${process.env.API_KEY}`
+    );
+    return res.data;
+  }
+);
+
+export const retrieveReviewBook = createAsyncThunk(
+  'product/retrieveReview',
+  async (param) => {
+    const colRef = collection(db, 'ulasan');
+    const snapshots = await getDocs(colRef);
+    const docs = await snapshots.docs.map((doc) => doc.data());
+    const filteringBook = await docs.filter(
+      (doc) => doc.bookId === param.bookId
+    );
+    return filteringBook;
+  }
+);
