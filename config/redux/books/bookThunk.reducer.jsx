d676b00cb23238de5189bd0db862c662236ce8d0
yahoogm <yaho.garfield@gmail.com>
2023-05-10 19:21:22 +0700

    Delete function to get review book

--- config/redux/books/bookThunk.reducer.jsx
+++ config/redux/books/bookThunk.reducer.jsx
@@ -1,7 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { db } from '@/config/firebase/sdk/sdk';
-import { collection, getDocs } from 'firebase/firestore';
 
 export const retrieveSearchBooks = createAsyncThunk(
   'product/retrieveSearch',
@@ -10,18 +8,5 @@ export const retrieveSearchBooks = createAsyncThunk(
       `${process.env.API_URL}/volumes?q=intitle:${param.search}&key=${process.env.API_KEY}`
     );
     return res.data;
-  }
-);
-
-export const retrieveReviewBook = createAsyncThunk(
-  'product/retrieveReview',
-  async (param) => {
-    const colRef = collection(db, 'ulasan');
-    const snapshots = await getDocs(colRef);
-    const docs = await snapshots.docs.map((doc) => doc.data());
-    const filteringBook = await docs.filter(
-      (doc) => doc.bookId === param.bookId
-    );
-    return filteringBook;
   }
 );
