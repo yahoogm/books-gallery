b8dc1f08080e6bb7d3cef47c065633f6380744ab
yahoogm <yaho.garfield@gmail.com>
2023-05-12 01:33:16 +0700

    Add effect to render data search book, if user first time open the web

--- components/organisme/CardContent/CardContent.organismModel.jsx
+++ components/organisme/CardContent/CardContent.organismModel.jsx
@@ -1,9 +1,19 @@
-import { useSearchBooksSelector, useSearchBooksTypeSelector } from '@/config/redux/books/bookSelector.reducer';
+import {
+  useSearchBooksSelector,
+  useSearchBooksTypeSelector,
+} from '@/config/redux/books/bookSelector.reducer';
 import { retrieveSearchBooks } from '@/config/redux/books/bookThunk.reducer';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 const useCardContentOrganismModel = () => {
+  const dispatch = useDispatch();
   const search = useSearchBooksSelector();
   const type = useSearchBooksTypeSelector();
+
+  useEffect(() => {
+    dispatch(retrieveSearchBooks({ search: 'programming' }));
+  }, []);
 
   const loading = type === retrieveSearchBooks.pending.type;
 
