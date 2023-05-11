1ac280f68f195a92a924751321c8ecb89a686d92
yahoogm <yaho.garfield@gmail.com>
2023-05-12 00:09:38 +0700

    Add method to make login with google is giving user to select the account

--- config/firebase/sdk/sdk.js
+++ config/firebase/sdk/sdk.js
@@ -18,3 +18,7 @@ export const db = getFirestore(app);
 export const auth = getAuth(app);
 
 export const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
