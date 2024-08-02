90b68742c842ba92b522d8251d611cbbea3f3130
yahoogm <yaho.garfield@gmail.com>
2024-08-02 11:47:12 +0700

    chore: handle for data cache

--- components/organisme/CardContent/CardContent.organismModel.jsx
+++ components/organisme/CardContent/CardContent.organismModel.jsx
@@ -2,9 +2,13 @@ import {
   useSearchBooksSelector,
   useSearchBooksTypeSelector,
 } from '@/config/redux/books/bookSelector.reducer';
+import { addSearchBooks } from '@/config/redux/books/bookSlice.reducer';
 import { retrieveSearchBooks } from '@/config/redux/books/bookThunk.reducer';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+
+const LOCAL_STORAGE_KEY = 'searchBooksData';
+const CACHE_DURATION = 1000 * 60 * 5;
 
 const useCardContentOrganismModel = () => {
   const dispatch = useDispatch();
@@ -12,10 +16,35 @@ const useCardContentOrganismModel = () => {
   const type = useSearchBooksTypeSelector();
 
   useEffect(() => {
+    const now = Date.now();
+
+    const storedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (storedData) {
+      const { data, timestamp } = JSON.parse(storedData);
+
+      if (now - timestamp < CACHE_DURATION) {
+        dispatch(addSearchBooks(data));
+        return;
+      }
+    }
+
     dispatch(retrieveSearchBooks({ search: 'programming' }));
   }, [dispatch]);
 
   const loading = type === retrieveSearchBooks.pending.type;
+
+  useEffect(() => {
+    if (type === retrieveSearchBooks.fulfilled.type) {
+      const now = Date.now();
+      localStorage.setItem(
+        LOCAL_STORAGE_KEY,
+        JSON.stringify({
+          data: search,
+          timestamp: now,
+        })
+      );
+    }
+  }, [search, type]);
 
   return { loading, search };
 };
